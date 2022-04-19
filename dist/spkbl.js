0476326be41a85deaf8a2a3d0bc45df211c6d20a
joschi <joschi@tollwerk.de>
2022-04-19 17:36:47 +0200

    Update compiled files

--- dist/spkbl.js
+++ dist/spkbl.js
@@ -1,4 +1,4 @@
-/* Speakable Text-To-Speech player 0.2.0 | https://github.com/tollwerk/speakable */
+/* Speakable Text-To-Speech player 0.4.0 | https://github.com/tollwerk/speakable */
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
